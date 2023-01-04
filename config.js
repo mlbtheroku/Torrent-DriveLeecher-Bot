d9258186189abb3d0699e8183f0b0bbe1d53ba93
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2023-01-04 21:14:22 +0630

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // const tgToken = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"; //aryantestbot
-const tgToken = "1237067579:AAGIFKPpyPk9FGD55SpKN6CgZaiW8iEj2Io"; //gtorrent
+const tgToken = "5845606249:AAH-CoS7NLQUOnI5JZvQcH6sNZIz79EnPA0"; //gtorrent
 const downloadFolder = "downloads";
-const mongoUrl = "mongodb+srv://aryanvikash:vikash12@cluster0-mp4rw.mongodb.net/test?retryWrites=true&w=majority"
+const mongoUrl = "mongodb+srv://aio:aio@aio.5z4gxok.mongodb.net/?retryWrites=true&w=majority"
 // const maxSize = 10;
 
 module.exports = { tgToken, downloadFolder, mongoUrl };
